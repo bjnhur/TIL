f13218e6f35a62c1fae46d7789bd0275956b4654
Bongjun Hur <bong@wiznet.io>
2020-02-04 15:08:07 +0900

    add pkg_path

--- conf.py
+++ conf.py
@@ -10,9 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+# theano_path = os.path.join(os.path.dirname(__file__), os.pardir)
+# sys.path.append(os.path.abspath(theano_path))
+pkg_path = os.path.abspath('..')
+sys.path.insert(0, pkg_path)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
